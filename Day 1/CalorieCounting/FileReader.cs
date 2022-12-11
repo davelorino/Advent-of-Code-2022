defe1425f83e195828e0f70650ae5f3cf4da32ad
DavideLorino <DavideLorino@CPC-David-9BB8I>
2022-12-11 12:57:43 +1100

    Add project files.

---
+++ Day 1/CalorieCounting/FileReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieCounting
+{
+    public class FileReader
+    {
+        public static string CalorieFileReader()
+        {
+            string inventory = System.IO.File.ReadAllText(@"C:\Users\DavideLorino\source\repos\Advent of Code 2022\Day 1\CalorieCounting\Day1Input.txt").Trim('\n');
+            return inventory;
+        }
+    }
+}
---
+++ Day 1/CalorieCounting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CalorieCounting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inventory = FileReader.CalorieFileReader();
+            var sum = inventory.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None)
+                .Select(f => f.Split(new string[] { "\r\n" }, StringSplitOptions.None)
+                    .Select(num => int.Parse(num.Trim()))
+                    .Sum()).ToList();
+            Console.WriteLine(sum.Max());
+            Console.WriteLine(sum.OrderByDescending(f => f).Take(3).Sum());
+        }
+    }
+}
